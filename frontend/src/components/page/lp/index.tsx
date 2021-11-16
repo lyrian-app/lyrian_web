0ca79b4174287c5020a8089f459f20e2c56047fe
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-16 23:39:27 +0900

    Create rough DOM structure
    The landing page will be basically created using a web design approach.

--- frontend/src/components/page/lp/index.tsx
+++ frontend/src/components/page/lp/index.tsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export const LandingPage = () => {
-  return <p>LandingPage</p>;
+  return (
+    <div className="l-lp">
+      <div className="l-top">
+        <div className="title"></div>
+        <div className="description"></div>
+        <div className="start_btn"></div> {/* TODO: Create btn ui component */}
+        <div className="down_arrow"></div>
+      </div>
+      <div className="l-what_is">
+        <div className="description"></div>
+        <div className="l-feature">
+          <div className="feat box"></div>
+          <div className="feat box"></div>
+        </div>
+      </div>
+      <div className="l-usage">
+        <div className="usage box"></div>
+        <div className="usage box"></div>
+        <div className="usage box"></div>
+      </div>
+      <div className="l-bottom">
+        <div className="phrase"></div>
+        <div className="start_btn"></div> {/* TODO: Create btn ui component */}
+      </div>
+      <footer className="l-footer">
+        <div className="l-icon_btn">
+          <div className="btn icon"></div>
+          <div className="btn icon"></div>
+        </div>
+        <div className="links"></div>
+        <div className="copyright"></div>
+      </footer>
+    </div>
+  );
 };
