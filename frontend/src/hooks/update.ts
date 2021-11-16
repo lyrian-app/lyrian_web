08555d87439273045252e510218b00a52ea8ea94
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-16 19:31:42 +0900

    Add update.ts

---
+++ frontend/src/hooks/update.ts
@@ -0,0 +1,41 @@
+import { Token, MarkovState, LyricsState } from "./types";
+
+type MarkovGeneratedMsg = {
+  type: "MarkovGeneratedMsg";
+  model: MarkovState;
+};
+
+type TokenFixedMsg = {
+  type: "TokenUpdatedMsg";
+  index: number;
+  token: Token;
+};
+
+type LyricsGeneretedMsg = {
+  type: "LyricsGeneratedMsg";
+  lyrics: string;
+};
+
+type MarkovMsg = MarkovGeneratedMsg | TokenFixedMsg;
+
+type LyricsMsg = LyricsGeneretedMsg;
+
+export const markovUpdate = (state: MarkovState, msg: MarkovMsg) => {
+  switch (msg.type) {
+    case "MarkovGeneratedMsg":
+      return msg.model;
+
+    case "TokenUpdatedMsg":
+      let newTokens = state.tokens;
+      newTokens[msg.index] = msg.token;
+      const newState = { ...state, tokens: newTokens };
+      return newState;
+  }
+};
+
+export const lyricsUpdate = (_state: LyricsState, msg: LyricsMsg) => {
+  switch (msg.type) {
+    case "LyricsGeneratedMsg":
+      return msg.lyrics;
+  }
+};
