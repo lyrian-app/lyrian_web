dd8d89cc37080d574d41cec17dfae5807d9790ee
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-17 22:42:04 +0900

    Create rough layout

--- frontend/src/components/page/lp/index.tsx
+++ frontend/src/components/page/lp/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { RectBtn, ScrollBtn } from "../../ui/buttons";
+import { RectBtn, ScrollBtn, IconBtn } from "../../ui/buttons";
 import { H1, H2 } from "../../ui/text";
 import style from "./style.module.scss";
 
@@ -20,28 +20,61 @@ export const LandingPage = () => {
 
       <div className={style.whatIs} ref={whatIsRef}>
         <H2>Lyrianとは？</H2>
-        <div className="description"></div>
-        <div className="l-feature">
-          <div className="feat box"></div>
-          <div className="feat box"></div>
+        <p className={style.description}>
+          Lyrianとは、マルコフ連鎖を用いて日本語の歌詞の生成を行うアプリケーションです。
+          お気に入りの文章を学習させることで、その文章に近い雰囲気の歌詞を生成することができます。
+        </p>
+        <div className={style.usageBox}>
+          <div className={style.usageItem}></div>
+          <div className={style.usageItem}></div>
+          <div className={style.usageItem}></div>
         </div>
       </div>
-      <div className="l-usage">
-        <div className="usage box"></div>
-        <div className="usage box"></div>
-        <div className="usage box"></div>
-      </div>
-      <div className="l-bottom">
-        <div className="phrase"></div>
-        <div className="start_btn"></div> {/* TODO: Create btn ui component */}
-      </div>
-      <footer className="l-footer">
-        <div className="l-icon_btn">
-          <div className="btn icon"></div>
-          <div className="btn icon"></div>
+
+      <div className={style.feature}>
+        <H2>特徴</H2>
+        <div className={style.featBox}>
+          <div className={style.featItem}></div>
+          <div className={style.featItem}></div>
         </div>
-        <div className="links"></div>
-        <div className="copyright"></div>
+      </div>
+
+      <div className={style.bottom}>
+        <H2>さあ、はじめよう。</H2>
+        <div className={style.startBtn}>
+          <RectBtn value="はじめる" size="large" />
+        </div>
+      </div>
+
+      <footer className={style.footer}>
+        <div className={style.iconBtns}>
+          <IconBtn iconName="icon-twitter" size="large" />
+          <IconBtn iconName="icon-github-circled" size="large" />
+        </div>
+        <div className={style.links}>
+          <a href="#" className={style.link}>
+            ドキュメント
+          </a>
+          <span className={style.bar}>|</span>
+          <a href="#" className={style.link}>
+            Q & A
+          </a>
+          <span className={style.bar}>|</span>
+          <a href="#" className={style.link}>
+            リリースノート
+          </a>
+          <span className={style.bar}>|</span>
+          <a href="#" className={style.link}>
+            お問い合わせ
+          </a>
+          <span className={style.bar}>|</span>
+          <a href="#" className={style.link}>
+            利用規約
+          </a>
+        </div>
+        <p className={style.copyright}>
+          Copyright © 2021 Ichi Hazuki and the Lyrian Project contributors.
+        </p>
       </footer>
     </div>
   );
