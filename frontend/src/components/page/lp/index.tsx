1309f4e6f6022006d8a6965ab8bb0b8abdc323d4
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-17 13:35:01 +0900

    Style top section

--- frontend/src/components/page/lp/index.tsx
+++ frontend/src/components/page/lp/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+import { RectBtn } from "../../ui/buttons";
+import style from "./style.module.scss";
+
 export const LandingPage = () => {
   return (
     <div className="l-lp">
-      <div className="l-top">
-        <div className="title"></div>
-        <div className="description"></div>
-        <div className="start_btn"></div> {/* TODO: Create btn ui component */}
+      <div className={style.top}>
+        <h1 className={style.title}>Lyrian Web</h1>
+        <p className={style.description}>日本語の歌詞生成アプリケーション</p>
+        <RectBtn value="はじめる" size="large" />
         <div className="down_arrow"></div>
       </div>
+
       <div className="l-what_is">
         <div className="description"></div>
         <div className="l-feature">
