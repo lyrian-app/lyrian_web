bee963f94b4d3357ceb13577be7d7e7eefc6f30b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-19 18:51:37 +0900

    Add modal of importing model

--- frontend/src/components/page/start/index.tsx
+++ frontend/src/components/page/start/index.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { RectBtn, StrBtn } from "../../ui/buttons";
 import { Footer } from "../../ui/footer";
 import { Textarea } from "../../ui/form";
-import { H2 } from "../../ui/text";
+import { Modal, ModalOverlay, ModalContent } from "../../ui/modal";
+import { H2, H3 } from "../../ui/text";
 import style from "./style.module.scss";
 
+const useModalState = (initValue: boolean): [boolean, () => void] => {
+  const [isOpen, setIsOpen] = useState(initValue);
+  const toggleModal = () => setIsOpen(!isOpen);
+  return [isOpen, toggleModal];
+};
+
 export const Start = () => {
+  const [isOpen, toggleModal] = useModalState(false);
+
   return (
     <div className={style.start}>
       <div className={style.main}>
@@ -28,8 +37,23 @@ export const Start = () => {
           <RectBtn value="作成" size="large" />
         </div>
 
-        <StrBtn value="既にモデルをお持ちの方はこちら" />
+        <StrBtn value="既にモデルをお持ちの方はこちら" onClick={toggleModal} />
       </div>
+
+      <Modal isOpen={isOpen}>
+        <ModalOverlay onClick={toggleModal} />
+        <ModalContent>
+          <H3>モデルのインポート</H3>
+          <p className={style.discription}>
+            作成したモデルファイルを選択してください。
+          </p>
+          <div className={style.fileInput}>
+            <input type="file" name="" id="" accept=".json" />
+          </div>
+          <RectBtn value="読み込み" size="medium" />
+        </ModalContent>
+      </Modal>
+
       <Footer />
     </div>
   );
