5d871763b52dc35b56ac4bc537a5104384412f8b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-20 15:48:55 +0900

    Implement form of creating Markov model

--- frontend/src/pages/start/index.tsx
+++ frontend/src/pages/start/index.tsx
@@ -1,4 +1,9 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { fetchMarkovModel } from "../../api";
+import { MarkovContext } from "../../providers";
+import { MarkovState } from "../../hooks";
 
 import { RectBtn, StrBtn } from "../../components/buttons";
 import { Footer } from "../../components/footer";
@@ -15,6 +20,24 @@ const useModalState = (initValue: boolean): [boolean, () => void] => {
 
 export const Start = () => {
   const [isOpen, toggleModal] = useModalState(false);
+  const [learningData, setText] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useContext(MarkovContext)!.dispatch;
+
+  const onTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.currentTarget.value);
+  };
+
+  const onSubmit = async () => {
+    await fetchMarkovModel(learningData).then((text) => {
+      const model: MarkovState = JSON.parse(text);
+      dispatch({ type: "MarkovGeneratedMsg", model: model });
+      navigate("/fix");
+    });
+    // TODO: Error handling of invalid response
+
+    return false;
+  };
 
   return (
     <div className={style.start}>
@@ -27,15 +50,26 @@ export const Start = () => {
           制作している楽曲の雰囲気に合わせた文章を入力することで、その雰囲気に近い歌詞を生成します。
         </p>
 
-        <div className={style.textarea}>
-          <Textarea placeholder="文章を入力してください。" />
-        </div>
+        <form className={style.form} target="create" onSubmit={onSubmit}>
+          <div className={style.textarea}>
+            <Textarea
+              placeholder="文章を入力してください。"
+              value={learningData}
+              onChange={onTextareaChange}
+              required={true}
+            />
+          </div>
 
-        <p className={style.notice}>※ 使用する文章の著作権にご注意ください。</p>
+          <p className={style.notice}>
+            ※ 使用する文章の著作権にご注意ください。
+          </p>
 
-        <div className={style.genModelBtn}>
-          <RectBtn value="作成" size="large" />
-        </div>
+          <div className={style.genModelBtn}>
+            <RectBtn value="作成" size="large" type="submit" />
+          </div>
+        </form>
+
+        <iframe className={style.iframe} name="create" />
 
         <StrBtn value="既にモデルをお持ちの方はこちら" onClick={toggleModal} />
       </div>
