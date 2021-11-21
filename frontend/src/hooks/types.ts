51bf115559ef2d03a0adaa0d378958786755a1e8
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-21 12:04:56 +0900

    Fix MarkovState type information to match server-side one

--- frontend/src/hooks/types.ts
+++ frontend/src/hooks/types.ts
@@ -1,28 +1,48 @@
 /**
  * A token of morphological analysis.
  */
-export interface Token {
+export interface StateSpace {
   word: string;
   mora: string;
   syllable: string;
-  partOfSpeech: string;
+  part_of_speech: PartOfSpeech;
+}
+
+/**
+ * An enumeration of part of speech.
+ */
+export enum PartOfSpeech {
+  副詞 = "副詞",
+  助動詞 = "助動詞",
+  助詞 = "助詞",
+  動詞 = "動詞",
+  名詞 = "名詞",
+  形容詞 = "形容詞",
+  感動詞 = "感動詞",
+  接続詞 = "接続詞",
+  接頭詞 = "接頭詞",
+  記号 = "記号",
+  連体詞 = "連体詞",
+  フィラー = "フィラー",
+  その他 = "その他",
+  未知語 = "未知語",
 }
 
 /**
  * A table of Walker's Alias Method.
  */
-interface WalkerTable {
-  aliases: Int32Array;
-  probs: Float32Array;
+export interface WaTable {
+  aliases: number[];
+  probs: number[];
 }
 
 /**
  * A state of Markov chain model.
  */
 export interface MarkovState {
-  tokens: Token[];
-  probsTable: WalkerTable[];
-  length: number;
+  state_space: StateSpace[];
+  wa_table: WaTable[];
+  prev_index: number;
 }
 
 /**
