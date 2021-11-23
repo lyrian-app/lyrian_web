90f54290c2853937b38866c52515ef543ed3c6df
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-23 10:19:23 +0900

    Add Discription component

---
+++ frontend/src/components/text/discription/Discription.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import style from "./discription.module.scss";
+
+interface DiscriptionProps {
+  children?: React.ReactNode;
+}
+
+export const Discription = ({ children }: DiscriptionProps) => {
+  return <p className={style.discription}>{children}</p>
+};
--- frontend/src/components/text/index.tsx
+++ frontend/src/components/text/index.tsx
@@ -1,1 +1,2 @@
+export * from "./discription/Discription";
 export * from "./headline/headline";
