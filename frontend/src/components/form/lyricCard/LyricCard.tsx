80563dad6c7d3b6a219252df9275466c58324ca9
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-25 18:42:26 +0900

    Add close button

--- frontend/src/components/form/lyricCard/LyricCard.tsx
+++ frontend/src/components/form/lyricCard/LyricCard.tsx
@@ -14,8 +14,7 @@ const useOpenState = (initValue: boolean): [boolean, () => void] => {
 
 export const LyricCard = (props: LyricCardProps) => {
   const [isOpen, toggle] = useOpenState(false);
-  const accordionBtnClass =
-    style.accordionBtn + (isOpen ? " icon-up-open" : " icon-down-open");
+  const iconName = isOpen ? " icon-up-open" : " icon-down-open";
   const bottomClass = `${style.bottom} ${isOpen ? style.isOpen : ""}`;
 
   return (
@@ -32,14 +31,13 @@ export const LyricCard = (props: LyricCardProps) => {
         <IconBtn iconName="icon-arrows-cw" size="medium" />
         <label className={style.label} htmlFor="numInput" >音数</label>
         <NumberInput defaultValue={3} max={20} min={1} />
-        <TextInput defaultValue="hoge" />
-        <label
-          className={accordionBtnClass}
-          htmlFor="accordion"
-          onClick={toggle}
-        >
-          <input name="accordion" type="checkbox" />
-        </label>
+        <TextInput />
+        <div className={style.accordionBtn}>
+          <IconBtn iconName={iconName} size="small" onClick={toggle} />
+        </div>
+        <div className={style.close}>
+          <IconBtn iconName="icon-cancel" size="small" />
+        </div>
       </div>
     </div>
   );
