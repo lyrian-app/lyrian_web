1a515c1b98a6483555d7a6a9af983742a843ce2c
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-25 18:48:47 +0900

    Add onChange handlers

--- frontend/src/components/form/lyricCard/LyricCard.tsx
+++ frontend/src/components/form/lyricCard/LyricCard.tsx
@@ -4,7 +4,13 @@ import { IconBtn } from "../../buttons";
 import { TextInput, Select, Option, NumberInput } from "..";
 import style from "./lyricCard.module.scss";
 
-interface LyricCardProps {}
+interface LyricCardProps {
+  onLyricGenerate?: React.MouseEventHandler<HTMLButtonElement>;
+  onNotesChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onLyricChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onUnitChange?: React.ChangeEventHandler<HTMLSelectElement>;
+  onClose?: React.MouseEventHandler<HTMLButtonElement>;
+}
 
 const useOpenState = (initValue: boolean): [boolean, () => void] => {
   const [isOpen, setIsOpen] = React.useState(initValue);
@@ -21,22 +27,46 @@ export const LyricCard = (props: LyricCardProps) => {
     <div className={style.lyricCard}>
       <div className={bottomClass}>
         <label htmlFor="unit">割り当て単位: </label>
-        <Select defaultValue="シラブル" name="unit">
+        <Select
+          defaultValue="シラブル"
+          name="unit"
+          onChange={props.onUnitChange}
+        >
           <Option value="シラブル">シラブル</Option>
           <Option value="モーラ">モーラ</Option>
         </Select>
       </div>
 
       <div className={style.top}>
-        <IconBtn iconName="icon-arrows-cw" size="medium" />
-        <label className={style.label} htmlFor="numInput" >音数</label>
-        <NumberInput defaultValue={3} max={20} min={1} />
-        <TextInput />
+        <IconBtn
+          iconName="icon-arrows-cw"
+          size="medium"
+          onClick={props.onLyricGenerate}
+        />
+
+        <label className={style.label} htmlFor="numInput">
+          音数
+        </label>
+        <NumberInput
+          name="numInput"
+          defaultValue={3}
+          max={20}
+          min={1}
+          onChange={props.onNotesChange}
+        />
+
+        <TextInput onChange={props.onLyricChange} />
+
         <div className={style.accordionBtn}>
           <IconBtn iconName={iconName} size="small" onClick={toggle} />
         </div>
+
         <div className={style.close}>
-          <IconBtn iconName="icon-cancel" size="small" />
+          <IconBtn
+            iconName="icon-cancel"
+            size="small"
+            onClick={props.onClose}
+          />
         </div>
       </div>
     </div>
