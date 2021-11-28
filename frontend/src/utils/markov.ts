06c7fc38201c9e8e089ba09bfac5d1ab0ea6a555
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-28 17:33:24 +0900

    Add markov module

---
+++ frontend/src/utils/markov.ts
@@ -0,0 +1,29 @@
+import { MarkovState } from "../hooks";
+import { Random, WaightedRand } from "./random";
+
+export class MarkovChain {
+  private markov: MarkovState;
+  private prevIdx: number;
+
+  constructor(markov: MarkovState) {
+    this.markov = markov;
+    this.prevIdx = markov.prev_index;
+  }
+
+  public next() {
+    const len = this.markov.state_space.length;
+
+    const row = (() => {
+      if (this.prevIdx === len) {
+        this.prevIdx = Random.randIntRange(0, len - 1);
+      }
+      return this.prevIdx;
+    })();
+
+    let waRand = new WaightedRand(this.markov.wa_table[row]);
+    const elemIdx = waRand.next();
+
+    this.prevIdx = elemIdx;
+    return this.markov.state_space[elemIdx];
+  }
+}
