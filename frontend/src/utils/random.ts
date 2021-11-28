d081818df66ec0568614eba80935e7dd648ecc07
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-28 17:33:12 +0900

    Add random module

---
+++ frontend/src/utils/random.ts
@@ -0,0 +1,30 @@
+import { WaTable } from "../hooks";
+
+export class Random {
+  public static randIntRange(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  public static randFloat() {
+    return Math.random();
+  }
+}
+
+export class WaightedRand {
+  private aliases;
+  private probs;
+
+  constructor(table: WaTable) {
+    this.aliases = table.aliases;
+    this.probs = table.probs;
+  }
+
+  public next() {
+    const i = Random.randIntRange(0, this.probs.length - 1);
+    const r = Random.randFloat();
+    if (r < this.probs[i]) {
+      return this.aliases[i];
+    }
+    return i;
+  }
+}
