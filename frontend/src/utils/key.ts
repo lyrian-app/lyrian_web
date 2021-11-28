f413caca6af9e35a09ff8720715af5c35d990af9
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-28 10:00:20 +0900

    Apply prettier

--- frontend/src/utils/key.ts
+++ frontend/src/utils/key.ts
@@ -1,4 +1,6 @@
 export const generateUniqueKey = () => {
-  const charCode = Array(8).fill(0).map(() => Math.floor(Math.random() * 65535));
+  const charCode = Array(8)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 65535));
   return String.fromCharCode(...charCode);
 };
