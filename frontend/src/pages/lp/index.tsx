874cbac83b16d937c165972c1dc115d35bffd6c5
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-30 21:46:17 +0900

    Design whatIs section

--- frontend/src/pages/lp/index.tsx
+++ frontend/src/pages/lp/index.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { RectBtn, ScrollBtn } from "../../components/buttons";
 import { Footer } from "../../components/footer";
 import { Grid } from "../../components/layout";
-import { H1, H2 } from "../../components/text";
+import { H1, H2, H3 } from "../../components/text";
 import style from "./style.module.scss";
 
 export const LandingPage = () => {
@@ -32,9 +32,38 @@ export const LandingPage = () => {
         </p>
         <div className={style.usageBox}>
           <Grid gap="2rem">
-            <div className={style.usageItem}></div>
-            <div className={style.usageItem}></div>
-            <div className={style.usageItem}></div>
+            <div className={style.usageItem}>
+              <div className={`${style.img} ${style.model}`} />
+              <div className={style.explain}>
+                <H3>モデル作成</H3>
+                <p>
+                  入力された文章を基にモデルを作成します。
+                  Lyrianでは、モデルに登録した単語を使用して歌詞を生成するため、
+                  モデル作成は歌詞の雰囲気を決める重要な工程とも言えます。
+                </p>
+              </div>
+            </div>
+            <div className={style.usageItem}>
+              <div className={`${style.img} ${style.pen}`} />
+              <div className={style.explain}>
+                <H3>作詞</H3>
+                <p>
+                  Lyrianは単語同士をランダムに組み合わせて歌詞を生成するため、
+                  普段では思いつかないようなフレーズを多く提案してくれます。
+                  もちろん、提案された歌詞を修正して使うこともできます。
+                </p>
+              </div>
+            </div>
+            <div className={style.usageItem}>
+              <div className={`${style.img} ${style.lyrics}`} />
+              <div className={style.explain}>
+                <H3>完成</H3>
+                <p>
+                  歌詞が完成すれば、後は歌うだけ。
+                  あなたの曲がより良いものになることを祈っています！
+                </p>
+              </div>
+            </div>
           </Grid>
         </div>
       </div>
