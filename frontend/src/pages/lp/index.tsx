0311fba538615585f0d5208b97e32190bbb18ab0
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-11-30 17:04:39 +0900

    Update LandingPage layout

--- frontend/src/pages/lp/index.tsx
+++ frontend/src/pages/lp/index.tsx
@@ -14,10 +14,10 @@ export const LandingPage = () => {
   const toStartPage = () => navigate("/start");
 
   return (
-    <div className="l-lp">
+    <div className={style.lp}>
       <div className={style.top}>
-        <H1>Lyrian Web</H1>
-        <p className={style.description}>日本語の歌詞生成アプリケーション</p>
+        <H1>Lyrian Web β</H1>
+        <p className={style.subtitle}>日本語の歌詞生成アプリケーション</p>
         <RectBtn value="はじめる" size="large" onClick={toStartPage} />
         <div className={style.scrollBtn}>
           <ScrollBtn jumpToRef={whatIsRef} arrow="down" />
@@ -26,7 +26,7 @@ export const LandingPage = () => {
 
       <div className={style.whatIs} ref={whatIsRef}>
         <H2>Lyrianとは？</H2>
-        <p className={style.description}>
+        <p className={style.discription}>
           Lyrianとは、マルコフ連鎖を用いて日本語の歌詞の生成を行うアプリケーションです。
           お気に入りの文章を学習させることで、その文章に近い雰囲気の歌詞を生成することができます。
         </p>
@@ -51,9 +51,7 @@ export const LandingPage = () => {
 
       <div className={style.bottom}>
         <H2>さあ、はじめよう。</H2>
-        <div className={style.startBtn}>
-          <RectBtn value="はじめる" size="large" onClick={toStartPage} />
-        </div>
+        <RectBtn value="はじめる" size="large" onClick={toStartPage} />
       </div>
 
       <Footer />
