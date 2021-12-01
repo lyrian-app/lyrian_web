eeec2b25e7e3641209469e2dc7b7ba8bf4ae177c
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-01 23:22:09 +0900

    Add sleep function

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -2,7 +2,13 @@ import { rest } from "msw";
 import { jsonModel } from "./sample_model.json.js";
 
 export const handlers = [
-  rest.post("/api/create_model", (_, res, ctx) => {
+  rest.post("/api/create_model", async (_, res, ctx) => {
+    const sleep = () => {
+      return new Promise((resolve) => setTimeout(() => resolve(), 3000, false));
+    };
+
+    await sleep();
+
     return res(ctx.status(200), ctx.json(jsonModel));
   }),
 ];
