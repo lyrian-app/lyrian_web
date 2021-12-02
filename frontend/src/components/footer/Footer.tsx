9a1f4c7c91a66be54b21f0e013662d659d6f3154
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-03 00:42:53 +0900

    Add URLs

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -5,23 +5,68 @@ import { IconBtn } from "../buttons";
 import style from "./footer.module.scss";
 
 export const Footer = () => {
+  const onTwitterClick = () => window.open("https://twitter.com");
+
+  const onGitHubClick = () =>
+    window.open("https://github.com/lyrian-app/lyrian_web");
+
   return (
     <footer className={style.footer}>
       <div className={style.iconBtns}>
-        <IconBtn iconName="icon-twitter" size="xlarge" color="white" />
-        <IconBtn iconName="icon-github-circled" size="xlarge" color="white" />
+        <IconBtn
+          iconName="icon-twitter"
+          size="xlarge"
+          color="white"
+          onClick={onTwitterClick}
+        />
+        <IconBtn
+          iconName="icon-github-circled"
+          size="xlarge"
+          color="white"
+          onClick={onGitHubClick}
+        />
       </div>
 
       <div className={style.links}>
-        <a href="#">ドキュメント</a>
+        <a
+          href="https://github.com/lyrian-app/lyrian_web/wiki/1.-%E4%BD%BF%E3%81%84%E6%96%B9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ドキュメント
+        </a>
         <span className={style.bar}>|</span>
-        <a href="#">Q & A</a>
+        <a
+          href="https://github.com/lyrian-app/lyrian_web/wiki/2.-Q-&-A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Q & A
+        </a>
         <span className={style.bar}>|</span>
-        <a href="#">リリースノート</a>
+        <a
+          href="https://github.com/lyrian-app/lyrian_web/blob/main/CHANGELOG.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          リリースノート
+        </a>
         <span className={style.bar}>|</span>
-        <a href="#">お問い合わせ</a>
+        <a
+          href="mailto:ichi.h3@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          お問い合わせ
+        </a>
         <span className={style.bar}>|</span>
-        <a href="#">利用規約</a>
+        <a
+          href="https://github.com/lyrian-app/lyrian_web/wiki/3.-%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%84"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          利用規約
+        </a>
       </div>
 
       <p className={style.copyright}>
