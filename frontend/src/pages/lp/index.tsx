aa54d382248d415e2488a8dc5daf8762a5a1f53f
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-03 18:30:00 +0900

    Update discription

--- frontend/src/pages/lp/index.tsx
+++ frontend/src/pages/lp/index.tsx
@@ -45,7 +45,7 @@ export const LandingPage = () => {
           <H2>Lyrianとは？</H2>
           <p className={style.discription}>
             Lyrianとは、マルコフ連鎖を用いて日本語の歌詞の生成を行うアプリケーションです。
-            お気に入りの文章を学習させることで、その文章に近い雰囲気の歌詞を生成することができます。
+            ユーザーが予め入力した文章の中から単語をランダムに選択し、歌詞生成を行います。
           </p>
           <div className={style.usageBox}>
             <Grid gap="2rem">
