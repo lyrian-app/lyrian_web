7d70e01e1698f3c5ab6246caf84d8aa889e33706
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-03 20:54:50 +0900

    Add Contact page

---
+++ frontend/src/pages/contact/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+import { Main } from "../../components/layout";
+import { MountStatus, Transition } from "../../components/transition";
+
+import style from "./style.module.scss";
+import content from "../../styles/content.module.scss";
+
+import mailImg from "../../assets/imgs/mail.png";
+
+export const Contact = () => {
+  const [status, setStatus] = useState<MountStatus>("willMount");
+
+  useEffect(() => {
+    if (status === "willMount") setStatus("mounted");
+  }, [status]);
+
+  return (
+    <Transition status={status}>
+      <div className={style.contact}>
+        <Main>
+          <div className={content.content}>
+            <h1>お問い合わせ</h1>
+
+            <p>ご連絡は以下のメールアドレス、または <a href="https://twitter.com/ichi_h3" target="_blank" rel="noopener noreferrer">Twitter</a> からお気軽にお問い合わせください（メールからのご連絡の方が確実です）。</p>
+
+            <div className={style.email}>
+              <img src={mailImg} alt="email" />
+            </div>
+          </div>
+        </Main>
+      </div>
+    </Transition>
+  );
+
+};
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Footer } from "../components/footer";
 
+import { Contact } from "../pages/contact";
 import { Docs } from "../pages/docs";
 import { Edit } from "../pages/edit";
 import { Fix } from "../pages/fix";
@@ -22,6 +23,7 @@ export const AppRoute = () => {
         <Route path="/lyrics" element={<Lyrics />} />
         <Route path="/docs" element={<Docs />} />
         <Route path="/q_a" element={<QA />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
