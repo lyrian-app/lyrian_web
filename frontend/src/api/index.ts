d000f0e1d16d198b95b26270cab4cf83c327fa8e
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-04 17:26:10 +0900

    Add request header

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,7 @@
 export const fetchMarkovModel = async (learningData: string) =>
   fetch("./api/create_model", {
     method: "POST",
+    headers: { "content-type": "application/x-www-form-urlencoded;charset=UTF-8" },
     body: `contents=${encodeURIComponent(learningData)}`,
   }).then(async (res) => {
     if (res.ok) {
