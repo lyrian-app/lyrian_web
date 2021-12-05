18fdf2d516ba53653a200f73b7dac7755304ce77
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-06 00:10:48 +0900

    Use dynamic port

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,6 @@
 use actix_files::NamedFile;
 use actix_web::{get, middleware, web, App, HttpServer, Result};
+use std::env;
 
 pub mod api;
 pub mod errors;
@@ -18,13 +19,18 @@ async fn index() -> Result<NamedFile> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8088".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
             .configure(routes)
             .default_service(web::route().to(index))
     })
-    .bind(("0.0.0.0", 8088))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
