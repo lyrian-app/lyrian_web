0cf599bc1b452180d8b092095a5af84649cbe961
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-06 16:54:13 +0900

    Unify message name

--- frontend/src/hooks/markov/update.ts
+++ frontend/src/hooks/markov/update.ts
@@ -5,13 +5,13 @@ type MarkovReadedMsg = {
   model: MarkovState;
 };
 
-type TokenFixedMsg = {
+type TokenUpdatedMsg = {
   type: "TokenUpdatedMsg";
   index: number;
   token: Token;
 };
 
-export type MarkovMsg = MarkovReadedMsg | TokenFixedMsg;
+export type MarkovMsg = MarkovReadedMsg | TokenUpdatedMsg;
 
 export const markovUpdate = (state: MarkovState, msg: MarkovMsg) => {
   switch (msg.type) {
