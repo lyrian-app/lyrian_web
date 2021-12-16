b529759e2a9ab888ea9a0d906cfcdf2c9b165fb1
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-16 22:55:43 +0900

    Check whether two objects have same value

--- frontend/src/utils/__test__/clone.test.ts
+++ frontend/src/utils/__test__/clone.test.ts
@@ -4,6 +4,7 @@ describe("cloneObj", () => {
   it("Two objects have same value, but do not share same reference", () => {
     const obj = { value: "something" };
     const cloned = cloneObj(obj);
+    expect(obj.value === cloned.value).toBe(true);
     expect(obj === cloned).toBe(false);
   });
 });
