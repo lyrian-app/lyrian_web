d422b7b677882fe06b7ff97919ed7f0a3340093d
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-17 08:09:25 +0900

    Remove unnecessary service

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,12 +1,10 @@
-use actix_files::Files;
 use actix_web::web;
 
 use crate::api::create_model;
 
 pub fn routes(config: &mut web::ServiceConfig) {
     config.service(
-        web::scope("")
-            .service(web::resource("/api/create_model").route(web::post().to(create_model)))
-            .service(Files::new("/", "/frontend/build").index_file("index.html")),
+        web::scope("/api")
+            .service(web::resource("/create_model").route(web::post().to(create_model))),
     );
 }
